422ec190faf0ba921faf354976e46eef3a1ac10c
jessekommandeur <jesse.k@live.nl>
2020-01-21 14:26:28 +0000

    setup for room functionalities

--- helpers.py
+++ helpers.py
@@ -1,9 +1,14 @@
 import requests
 import urllib.parse
 import os
+from cs50 import SQL
+from random import randint
 
 from flask import redirect, render_template, request, session
 from functools import wraps
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///spotiwy.db")
 
 
 def apology(message, code=400):
@@ -33,3 +38,31 @@ def login_required(f):
             return redirect("/homepage")
         return f(*args, **kwargs)
     return decorated_function
+
+
+def generatenumber():
+
+    """generates a random roomnumber"""
+
+    # generate roomnumber
+    roomnumber = randint(100000, 999999)
+
+    while db.execute("SELECT * from rooms WHERE roomnumber = :roomnumber", roomnumber = roomnumber):
+        roomnumber = randint(100000, 999999)
+
+    return roomnumber
+
+# def numbercheck(roomname):
+
+    # """checks is user input is valid"""
+
+    # # Ensure user enters 6 digit number
+    # if not len(request.form.get("roomnumber")) == 6:
+    #     return apology("should enter 6 digit number", 400)
+
+    # # Ensure user enters digits only
+    # if not request.form.get("roomnumber").isdigit():
+    #     return apology("should enter digits only number", 400)
+
+    # if not db.execute("SELECT * FROM rooms WHERE roomnumber = :roomnumber", roomnumber = roomnumber):
+    #     return apology("room does not exist", 400)
