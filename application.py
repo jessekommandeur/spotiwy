e76bd63fb3a662cd6d48495f77599139e5dd09fe
Koen099 <koenberenschot@gmail.com>
2020-01-21 12:39:03 +0000

    host.html infrastructure changes

--- application.py
+++ application.py
@@ -220,7 +220,7 @@ def host():
     #User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
-        "TODO"
+        #
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
